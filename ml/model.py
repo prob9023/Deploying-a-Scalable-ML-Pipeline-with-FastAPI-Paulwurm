1c3b9123af48001ce4cfcf01e6eea17dc84ac262
Paul Aulwurm <paulaulwurm@gmail.com>
2026-05-19 09:59:52 -0400

    finished performance_on_categorical_slice function and added encoder and lb

--- ml/model.py
+++ ml/model.py
@@ -1,8 +1,11 @@
 import pickle
+import sklearn
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
 from sklearn.ensemble import RandomForestClassifier
+#importing for performance_on_categorical_slice()
+from sklearn.preprocessing import OneHotEncoder, LabelBinarizer
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -123,11 +126,18 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
+    #setting the slice of data to a specific column value
+    sliced_data = data[data[column_name]] == slice_value
+
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+        #pass in functions values
+        X=sliced_data,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = model.predict(X_slice) # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
