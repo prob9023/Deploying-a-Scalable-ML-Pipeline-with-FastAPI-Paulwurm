ec4b30ea94d5fe1d635840ebff1c09da1f39cedd
Paul Aulwurm <paulaulwurm@gmail.com>
2026-05-19 14:26:36 -0400

    added last 2 unit tests

--- test_ml.py
+++ test_ml.py
@@ -3,6 +3,7 @@ import pytest
 import numpy as np
 from ml.model import inference
 from sklearn.ensemble import RandomForestClassifier
+from ml.model import train_model, compute_model_metrics
 
 # TODO: implement the first test. Change the function name and input as needed
 def test_inference_array():
@@ -21,18 +22,36 @@ def test_inference_array():
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_uses_rf():
     """
-    # add description for the second test
+    this test ensures the RandomForest algorithm is used
     """
-    # Your code here
-    pass
+    #mock data
+    X = np.array([[1,2], [3,4]])
+    y = np.array([0,1])
+
+    model = train_model(X, y)
+
+    assert isinstance(model, RandomForestClassifier)
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_divide_zero():
     """
-    # add description for the third test
+    test if we handle worst case scenario of zero positives in compute_model_metrics function
+
+    because we set zero_division=1 we should assert 1.0 = 1.0
     """
-    # Your code here
-    pass
+    #mock data
+    true = np.array([1, 1, 1, 1])
+    pred = np.array([0, 0, 0, 0])
+    
+    #compute_model_metrics
+    precision, recall, fbeta = compute_model_metrics(true, pred)
+
+    #should be one for precision
+    assert precision == 1.0
+    #should be zero for recall (opposite)
+    assert recall == 0.0
+    #should be 0 for fscore because recall is 0 and precision * 0 = 0
+    assert fbeta == 0.0
